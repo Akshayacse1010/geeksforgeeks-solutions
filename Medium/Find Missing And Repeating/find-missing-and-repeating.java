26dfe3cdde260a7038d4a5b00eeaeae562c59c9d
Akshaya S <116668612+Akshayacse1010@users.noreply.github.com>
2024-04-17 21:20:13 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Missing And Repeating/find-missing-and-repeating.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a, n);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        // code here
+        int xr = 0;
+        for(int i= 0; i < n; i++){
+            xr = xr ^ arr[i];
+            xr = xr ^ (i + 1);
+        }
+        int number = (xr & ~(xr - 1));
+        
+        int zero = 0;
+        int one = 0;
+        for(int i =0; i < n; i++){
+            if((arr[i] & number ) != 0){
+                one = one ^ arr[i];
+            }
+            else{
+                zero = zero ^ arr[i];
+            }
+        }
+        for(int i=  1 ;i <=n ;i++){
+            if((i & number) != 0){
+                one = one ^ i;
+            }
+            else{
+                zero = zero ^ i;
+            }
+        }
+        for(int i =0 ; i < n ;i++){
+            if(arr[i] == zero){
+                return new int[] {zero , one};
+            }
+        }
+        return new int[] {one , zero};
+    }
+}
